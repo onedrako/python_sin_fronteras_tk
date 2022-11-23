3807c24982cd9374e587b0d8c2fdf6582fc8be6e
onedrako <gabrielhv@onedrako.com>
2022-11-23 12:48:45 -0600

    Clase 218. Creando la interfaz

--- db.py
+++ db.py
@@ -16,3 +16,29 @@ c.execute("""CREATE TABLE IF NOT EXISTS todo (
 """)
 
 conn.commit()
+
+def addTodo():
+    todo = e.get()
+    c.execute("""
+      INSERT INTO todo (description, completed) VALUES (?, ?)
+    """, (todo, False))
+    conn.commit()
+    e.delete(0, END)
+
+l = Label(root, text="tarea")
+l.grid(row=0, column=0)
+
+e = Entry(root, width=40)
+e.grid(row=0, column=1)
+
+btn = Button(root, text="Agregar", command=addTodo)
+btn.grid(row=0, column=2)
+
+frame = LabelFrame(root, text="Mis Tareas", padx=5, pady=5)
+frame.grid(row=1, column=0, columnspan=3, sticky="nsew", padx=5)
+
+e.focus()
+
+root.bind("<Return>", lambda x: addTodo())
+
+root.mainloop()
